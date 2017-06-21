382ae81fb3d355d18adb53f226f5a61577eb7b33
liujunyang <liujunyangscript@gmail.com>
2017-06-21 16:40:47 +0800

    fix在缩略图等蒙版时点击工具栏更多按钮

--- src/components/teacher/template/toolbar.vue
+++ src/components/teacher/template/toolbar.vue
@@ -41,10 +41,9 @@
 
   export default {
     name: 'Tollbar',
-    props: ['lessonid', 'socket', 'newdoubt', 'newtougao'],
+    props: ['lessonid', 'socket', 'newdoubt', 'newtougao', 'isToolbarMoreBoxHidden'],
     data () {
       return {
-        isToolbarMoreBoxHidden: true,           // 工具栏更多按钮们的隐藏
       }
     },
     created () {
@@ -81,7 +80,7 @@
       toggleToolbarMoreBox () {
         let self = this
 
-        self.isToolbarMoreBoxHidden = !self.isToolbarMoreBoxHidden
+        self.$emit('update:isToolbarMoreBoxHidden', !self.isToolbarMoreBoxHidden)
       },
       /**
        * 点击二维码按钮，发送弹出二维码控制面板的请求，收到回复后在回复中才打开面板
@@ -123,7 +122,7 @@
   .toolbar-root {
     position: relative;
   }
-  
+
   .rc-toolbar {
     display: flex;
     align-items: center;
