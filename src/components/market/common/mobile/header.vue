d685dd656aa57015e97ce4d9b9bc52cc6212c2a1
chouzhenyi <zzyatbeijing@163.com>
2017-09-07 17:48:58 +0800

    style

--- src/components/market/common/mobile/header.vue
+++ src/components/market/common/mobile/header.vue
@@ -34,8 +34,8 @@
   @import "~@/style/market/insert.css";
   .nav {position: fixed; z-index: 50; width: 100%; left: 0; top: 0; color: #fff;}
   .nav .bg {position: absolute; left: 0; top: 0; width: 100%; height: 100%; background-color: #282C2F; display: none;}
-  .nav .box {position: relative; margin: 0 auto; height: 45px;}
-  .nav .box .logo {float: left; width: 100px; margin-left: 10px; margin-top: 10px;}
-  .nav .box .right {float: right; margin-right: 15px; line-height: 45px; font-size: 14px;}
+  .nav .box {position: relative; margin: 0 auto; height: .9rem;}
+  .nav .box .logo {float: left; width: 2rem; margin-left: .2rem; margin-top: .2rem;}
+  .nav .box .right {float: right; margin-right: .3rem; line-height: .9rem; font-size: .3rem;}
   .nav .box a {color: #fff;}
 </style>
--- src/components/market/mobile_market.vue
+++ src/components/market/mobile_market.vue
@@ -1,28 +1,101 @@
 <template>
     <div class="app">
-      <ul class="con-width  text-center">
-        <li class="over inline-block" v-for="i in list">
-          <div class="img over">
-            <img :src="i.cover"/>
+      <div class="color3 list-name">
+        雨课件市场
+      </div>
+      <ul class="con-width">
+        <li class="over text-center inline-block" v-for="i in list">
+          <div class="container">
+            <div class="img over">
+              <img :src="i.cover"/>
+            </div>
+            <div class="text-con">
+              <div class="color0 text-left over name">
+                {{i.title}}
           </div>
-          <div class="text-con">
-            <div class="color0 text-left over name">
-              {{i.title}}
+              <div class="color9b text-left author">
+                {{i.school}} {{i.author}}
           </div>
-            <div class="color9b text-left author">
-              {{i.school}} {{i.author}}
+              <div class="price">
+                ¥ {{i.discount_price}}
           </div>
-            <div class="price">
-              ¥ {{i.discount_price}}
+              <div class="font14 old-price">
+                ¥ {{i.price}}
           </div>
-            <div class="font14 old-price">
-              ¥ {{i.price}}
-          </div>
-            <a :href="i.buy_link" class="inline-block font16 buy" target = _blank>立即购买</a>
+              <a :href="i.buy_link" class="inline-block font16 buy" target = _blank>立即购买</a>
+            </div>
           </div>
         </li>
-        <li class="inline-block expect">
-          <div>
+        <li class="inline-block text-center expect">
+          <div class="container">
+            <div class="over img-con">
+              <i class="iconfont icon-logo-book"></i>
+            </div>
+            <div class="font12 colora text-left text">
+              <p>雨课堂盼望熟练使用雨课堂，在各学科领域具有丰富教学经验的老师出品自己的雨课件，以共建共享更加完善的教学方案体系。</p>
+              <p>请联系客服获取更多相关信息。</p>
+              <p>客服邮箱：<span class="color6">yuketang@xuetangx.com</span></p>
+            </div>
+          </div>
+        </li>
+        <li class="over text-center inline-block" v-for="i in list">
+          <div class="container">
+            <div class="img over">
+              <img :src="i.cover"/>
+            </div>
+            <div class="text-con">
+              <div class="color0 text-left over name">
+                {{i.title}}
+          </div>
+              <div class="color9b text-left author">
+                {{i.school}} {{i.author}}
+          </div>
+              <div class="price">
+                ¥ {{i.discount_price}}
+          </div>
+              <div class="font14 old-price">
+                ¥ {{i.price}}
+          </div>
+              <a :href="i.buy_link" class="inline-block font16 buy" target = _blank>立即购买</a>
+            </div>
+          </div>
+        </li>
+        <li class="inline-block text-center expect">
+          <div class="container">
+            <div class="over img-con">
+              <i class="iconfont icon-logo-book"></i>
+            </div>
+            <div class="font12 colora text-left text">
+              <p>雨课堂盼望熟练使用雨课堂，在各学科领域具有丰富教学经验的老师出品自己的雨课件，以共建共享更加完善的教学方案体系。</p>
+              <p>请联系客服获取更多相关信息。</p>
+              <p>客服邮箱：<span class="color6">yuketang@xuetangx.com</span></p>
+            </div>
+          </div>
+        </li>
+        <li class="over text-center inline-block" v-for="i in list">
+          <div class="container">
+            <div class="img over">
+              <img :src="i.cover"/>
+            </div>
+            <div class="text-con">
+              <div class="color0 text-left over name">
+                {{i.title}}
+          </div>
+              <div class="color9b text-left author">
+                {{i.school}} {{i.author}}
+          </div>
+              <div class="price">
+                ¥ {{i.discount_price}}
+          </div>
+              <div class="font14 old-price">
+                ¥ {{i.price}}
+          </div>
+              <a :href="i.buy_link" class="inline-block font16 buy" target = _blank>立即购买</a>
+            </div>
+          </div>
+        </li>
+        <li class="inline-block text-center expect">
+          <div class="container">
             <div class="over img-con">
               <i class="iconfont icon-logo-book"></i>
             </div>
@@ -67,89 +140,104 @@
     min-height: 8rem;
     background-color: #fff;
     margin-top: .9rem;
+    padding: 0 .2rem;
     .con-width{
       width: auto;
     }
+    .list-name{
+      font-size: .32rem;
+      height: .8rem;
+      line-height: .8rem;
+      margin-top: .2rem;
+    }
     ul {
+      font-size: 0;
+      padding-bottom: .4rem;
       li {
-        width: 3.44rem;
-        height: 4.6rem;
+        width: 50%;
+        height: 4.8rem;
         background-color: #fff;
-        border: .02rem solid #c8c8c8;
-        margin: .2rem .2rem 0 .2rem;
-        background-color: #fff;
-        .img {
-          width: 100%;
-          height: 2rem;
-          img {
+        padding-right: .2rem;
+        box-sizing: border-box;
+        margin-top: .2rem;
+        div.container{
+          height: 100%;
+          border: .02rem solid #c8c8c8;
+          .img {
             width: 100%;
+            height: 2rem;
+            img {
+              width: 100%;
+            }
           }
-        }
-        .text-con{
-          width: 100%;
-          padding:.1rem;
-          font-size: .32rem;
-          line-height: .5rem;
-          .name {
+          .text-con{
             width: 100%;
-            text-overflow: ellipsis;
-            padding-left: .1rem;
-            height: .5rem;
-            display: -webkit-box;
-            -webkit-box-orient: vertical;
-            -webkit-line-clamp: 2;
-            overflow: hidden;
-          }
-          .author {
-            height: .28rem;
-            width: 100%;
-            border-bottom: .02rem solid #c8c8c8;
-            margin: 0 auto;
-            padding: 0px 0px .5rem .1rem;
-            line-height: .28rem;
-          }
-          .price {
-            height: .4rem;
-            color: #F5A623;
-          }
-          .old-price{
-            font-size: .3rem;
-            line-height: .4rem;
-            color: #9b9b9b;
-            text-decoration: line-through;
-            text-decoration-color: #979797;
-          }
-          a {
-            background-color: #F5A623;
-            color: #fff;
-            width: 1.5rem;
-            height: .5rem;
-            border-radius: .4rem;
+            padding:.1rem;
+            font-size: .32rem;
             line-height: .5rem;
-            font-size: .28rem;
-            margin: .1rem auto;
+            .name {
+              width: 100%;
+              text-overflow: ellipsis;
+              padding-left: .1rem;
+              height: .5rem;
+              display: -webkit-box;
+              -webkit-box-orient: vertical;
+              -webkit-line-clamp: 2;
+              overflow: hidden;
+            }
+            .author {
+              height: .28rem;
+              width: 100%;
+              border-bottom: .02rem solid #c8c8c8;
+              margin: 0 auto;
+              padding: 0px 0px .5rem .1rem;
+              line-height: .28rem;
+            }
+            .price {
+              height: .4rem;
+              color: #F5A623;
+            }
+            .old-price{
+              font-size: .3rem;
+              line-height: .4rem;
+              color: #9b9b9b;
+              text-decoration: line-through;
+              text-decoration-color: #979797;
+            }
+            a {
+              background-color: #F5A623;
+              color: #fff;
+              width: 1.5rem;
+              height: .5rem;
+              border-radius: .4rem;
+              line-height: .5rem;
+              font-size: .28rem;
+              margin: .1rem auto;
+            }
           }
         }
-      }
-      li:nth-child(2){
-        margin-right: 0;
       }
       li.expect{
         font-size: .2rem;
-        .img-con{
-          width: 100%;
-          height: 2rem;
-          margin-bottom: .2rem;
-          line-height: 2rem;
-          background-color: #B1CEF9;
-          i{
-            font-size: 1.8rem;
-            color: rgba(255,255,255,.3);
+        div.container{
+          .img-con{
+            width: 100%;
+            height: 2rem;
+            margin-bottom: .2rem;
+            line-height: 2rem;
+            background-color: #B1CEF9;
+            i{
+              font-size: 1.8rem;
+              color: rgba(255,255,255,.3);
+            }
+          }
+          .text{
+            padding:0 .1rem;
           }
         }
-        .text{
-          padding:0 .1rem;
-        }
+      }
+      li:nth-child(even){
+        padding: 0;
       }
     }
   }
