3e708d8dd90364a846eee9c842ca8020f9b622eb
chouzhenyi <zzyatbeijing@163.com>
2017-09-07 16:19:26 +0800

    style

--- src/components/market/mobile_market.vue
+++ src/components/market/mobile_market.vue
@@ -5,31 +5,29 @@
           <div class="img over">
             <img :src="i.cover"/>
           </div>
-          <div class="color0 text-left over name">
-            {{i.title}}
+          <div class="text-con">
+            <div class="color0 text-left over name">
+              {{i.title}}
           </div>
-          <div class="font18 color9b text-left author">
-            {{i.school}} {{i.author}}
+            <div class="color9b text-left author">
+              {{i.school}} {{i.author}}
           </div>
-          <div class="color4a font20 describe">
-            <!--{{i.introduce}}-->
+            <div class="price">
+              ¥ {{i.discount_price}}
           </div>
-          <div class="font20 price">
-            ¥ {{i.discount_price}}
+            <div class="font14 old-price">
+              ¥ {{i.price}}
           </div>
-          <div class="font14 old-price">
-            ¥ {{i.price}}
+            <a :href="i.buy_link" class="inline-block font16 buy" target = _blank>立即购买</a>
           </div>
-          <a :href="i.buy_link" class="inline-block font16 buy" target = _blank>立即购买</a>
         </li>
         <li class="inline-block expect">
           <div>
             <div class="over img-con">
-              <img src="~images/market/icon/expect.png"/>
+              <i class="iconfont icon-logo-book"></i>
             </div>
             <div class="font12 colora text-left text">
               <p>雨课堂盼望熟练使用雨课堂，在各学科领域具有丰富教学经验的老师出品自己的雨课件，以共建共享更加完善的教学方案体系。</p>
-
               <p>请联系客服获取更多相关信息。</p>
               <p>客服邮箱：yuketang@xuetangx.com</p>
             </div>
@@ -66,85 +64,92 @@
 <style lang="scss" scoped>
   .app{
     width: 100%;
-    min-height: 400px;
+    min-height: 8rem;
     background-color: #fff;
-    margin-top: 45px;
+    margin-top: .9rem;
     .con-width{
-      width: 354px;
+      width: auto;
     }
     ul {
       li {
-        width: 172px;
-        height: 230px;
+        width: 3.44rem;
+        height: 4.6rem;
         background-color: #fff;
-        border: 1px solid #c8c8c8;
-        margin: 10px 10px 0 10px;
+        border: .02rem solid #c8c8c8;
+        margin: .2rem .2rem 0 .2rem;
+        background-color: #fff;
         .img {
           width: 100%;
-          height: 102px;
+          height: 2rem;
           img {
             width: 100%;
           }
+          background-color: #ffae00;
         }
-        .name {
+        .text-con{
           width: 100%;
-          text-overflow: ellipsis;
-          padding-left: 10px;
-          padding-top: 5px;
-          line-height: 10px;
-          max-height: 24px;
-          display: -webkit-box;
-          -webkit-box-orient: vertical;
-          -webkit-line-clamp: 2;
-          overflow: hidden;
-          font-size: 1rem;
-        }
-        .author {
-          height: 14px;
-          width: 100px;
-          border-bottom: 1px solid #c8c8c8;
-          margin: 0 auto;
-          padding: 0px 0px 18px 5px;
-          line-height: 14px;
-        }
-        .describe {
-          height: 10px;
-          line-height: 10px;
-        }
-        .price {
-          height: 14px;
-          line-height: 14px;
-          color: #F5A623;
-        }
-        .old-price{
-          height: 10px;
-          line-height: 10px;
-          color: #9b9b9b;
-          text-decoration: line-through;
-          text-decoration-color: #979797;
-        }
-        a {
-          background-color: #F5A623;
-          color: #fff;
-          width: 64px;
-          height: 18px;
-          border-radius: 9px;
-          line-height: 18px;
-          margin: 5px auto;
+          padding:.1rem;
+          font-size: .32rem;
+          line-height: .5rem;
+          .name {
+            width: 100%;
+            text-overflow: ellipsis;
+            padding-left: .1rem;
+            height: .5rem;
+            display: -webkit-box;
+            -webkit-box-orient: vertical;
+            -webkit-line-clamp: 2;
+            overflow: hidden;
+          }
+          .author {
+            height: .28rem;
+            width: 100%;
+            border-bottom: .02rem solid #c8c8c8;
+            margin: 0 auto;
+            padding: 0px 0px .5rem .1rem;
+            line-height: .28rem;
+          }
+          .price {
+            height: .4rem;
+            color: #F5A623;
+          }
+          .old-price{
+            font-size: .3rem;
+            line-height: .4rem;
+            color: #9b9b9b;
+            text-decoration: line-through;
+            text-decoration-color: #979797;
+          }
+          a {
+            background-color: #F5A623;
+            color: #fff;
+            width: 1.5rem;
+            height: .5rem;
+            border-radius: .4rem;
+            line-height: .5rem;
+            font-size: .28rem;
+            margin: .1rem auto;
+          }
         }
       }
       li:nth-child(2){
         margin-right: 0;
       }
       li.expect{
-        background-color: #f8f8f8;
+        font-size: .2rem;
         .img-con{
           width: 100%;
-          height: 136px;
-          margin-bottom: 10px;
+          height: 2rem;
+          margin-bottom: .2rem;
+          line-height: 2rem;
+          background-color: #B1CEF9;
+          i{
+            font-size: 1.8rem;
+            color: rgba(177, 206, 249, .4);
+          }
         }
         .text{
-          padding:0 10px;
+          padding:0 .1rem;
         }
       }
     }
