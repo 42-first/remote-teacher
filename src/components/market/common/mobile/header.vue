5734656b181ef75f43d74ff0b3f9cd30db2ca545
chouzhenyi <zzyatbeijing@163.com>
2017-09-27 15:28:37 +0800

    header

--- src/components/market/common/mobile/header.vue
+++ src/components/market/common/mobile/header.vue
@@ -1,15 +1,14 @@
 <template>
-  <nav id="nav" class="nav">
-    <div class="box clearfix">
-      <a href="/">
-        <img class="logo" src="http://sfe.ykt.io/o_1bpfrptlcaflochlas11h1s1c9.png" alt="logo">
-      </a>
-      <a href="/help" class="right">帮助</a>
-      <a class="right" onclick="javascript:alert('请在电脑上打开 \n ykt.io \n下载雨课堂软件');">下载</a>
-      <a href="/lesson/market/mobile_market" class="right active">雨课件</a>
-      <a href="/presenter" class="right">翻页笔</a>
+  <!-- 导航模块 -->
+  <div class="nav">
+    <a class="logo-wrapper" href="/"><img class="logo" src="http://sfe.ykt.io/o_1bpfrptlcaflochlas11h1s1c9.png" alt="logo"></a>
+    <div class="nav-menus">
+      <a class="join-us" href="/presenter">翻页笔</a>
+      <a class="join-us curr" href="/lesson/market/mobile_market">雨课件</a>
+      <a class="download" onclick="javascript:alert('请在电脑上打开 \n ykt.io \n下载雨课堂软件');">下载</a>
+      <a class="help" href="/help" >帮助</a>
     </div>
-  </nav>
+  </div>
 </template>
 
 <script>
@@ -30,27 +29,35 @@
   }
 </script>
 <style lang="scss" scoped>
-  @import "~@/style/market/insert.css";
   .nav {
-    position: fixed;
-    z-index: 50;
+    z-index: 1;
+    position: fixed; left: 0; top: 0;
+    display: flex; align-items: center; justify-content: space-between;
     width: 100%;
-    left: 0;
-    top: 0;
-    color: #fff;
-    background-color: #282C2F;
-    height: .8rem;
-    line-height: .8rem;
-    font-size: 0;
+    color: #9B9B9B;
+    background: #282C2F;
     -webkit-font-smoothing: subpixel-antialiased;
   }
-  .nav .box {position: relative; margin: 0 auto;
-    padding-top:1px;
+
+  .nav .logo-wrapper {
+    margin-left: 15px;
   }
-  .nav .box .logo { width: 1.3rem; margin-left: .3rem;line-height: .8rem;vertical-align: middle;margin-top: -2.5px;}
-  .nav .box .right {float: right; margin-right: .2rem;font-size: .24rem;}
-  .nav .box a {color: #c8c8c8;display: inline-block;height: 100%;vertical-align:bottom;}
-  .nav .box a.active{
-    color: #fff;
+  .nav .logo {
+    display: block;
+    width: 65px;
   }
+
+  .nav-menus {
+    display: block;
+    display: flex; align-items: center; justify-content: flex-end;
+  }
+
+  .nav a {
+    margin-right: 10px;
+    line-height: 40px;
+    font-size: 12px;
+    color: #C8C8C8;
+    vertical-align: bottom;
+  }
+  .nav  a.curr { color: #fff; }
 </style>
