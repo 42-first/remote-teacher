fd6195ecb39a7cd9723b8995b67a5cf630c35875
chouzhenyi <zzyatbeijing@163.com>
2017-09-29 18:30:52 +0800

    link

--- src/router/market.js
+++ src/router/market.js
@@ -7,7 +7,25 @@ import courseware from '@/components/market/courseware'
 
 Vue.use(Router)
 
-export default new Router({
+var browser = {
+  versions: (function () {
+    var u = navigator.userAgent
+    return {
+      trident: u.indexOf('Trident') > -1,
+      presto: u.indexOf('Presto') > -1,
+      webKit: u.indexOf('AppleWebKit') > -1,
+      gecko: u.indexOf('Gecko') > -1 && u.indexOf('KHTML') == -1,
+      mobile: !!u.match(/AppleWebKit.*Mobile.*/),
+      ios: !!u.match(/\(i[^;]+;( U;)? CPU.+Mac OS X/),
+      android: u.indexOf('Android') > -1 || u.indexOf('Linux') > -1,
+      iPhone: u.indexOf('iPhone') > -1,
+      iPad: u.indexOf('iPad') > -1,
+      webApp: u.indexOf('Safari') === -1
+    }
+  })()
+}
+
+var router = new Router({
   base: process.env.NODE_ENV === 'production' ? '/lesson/market' : '/',
   mode: process.env.NODE_ENV === 'production' ? 'history' : 'hash',
   routes: [
@@ -43,3 +61,19 @@ export default new Router({
     }
   ]
 })
+
+router.beforeEach((to, from, next) => {
+  if (to.name === 'market' || to.name === 'courseware') {
+    if (browser.versions.mobile || browser.versions.ios || browser.versions.android || browser.versions.iPhone || browser.versions.iPad){
+      next({path: '/mobile/courseware'})
+    }
+  }
+  if (to.name === 'mobile_courseware' || to.name === 'mobile_market') {
+    if (!(browser.versions.mobile || browser.versions.ios || browser.versions.android || browser.versions.iPhone || browser.versions.iPad)){
+      next({path: '/'})
+    }
+  }
+  next()
+})
+
+export default router
