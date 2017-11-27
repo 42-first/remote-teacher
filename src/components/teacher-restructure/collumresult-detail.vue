6fc3429b9a9bc5ea5e5f553d1a859cfb80afbb22
liujunyang <liujunyangscript@gmail.com>
2017-11-27 11:02:25 +0800

    fix匿名投票

--- src/components/teacher-restructure/collumresult-detail.vue
+++ src/components/teacher-restructure/collumresult-detail.vue
@@ -13,7 +13,7 @@
 	    <div class="choice-list">
 	      <div class="choice-item" v-for="(choiceItem, index) in problemResultDetailData.data">
 	      	<v-touch class="item-hd" v-on:tap="toggleChoiceItem(index)">
-	      		<i v-show="problemResultDetailData.problem_type !== 3" :class="['iconfont', 'f20', choiceItem.label === problemResultDetailData.answer ? 'icon-correct' : 'icon-wrong']"></i>
+	      		<i v-show="problemResultDetailData.problem_type !== 3 && problemResultDetailData.problem_type !== 8" :class="['iconfont', 'f20', choiceItem.label === problemResultDetailData.answer ? 'icon-correct' : 'icon-wrong']"></i>
 	      		<span class="f18 asw">{{choiceItem.label}}</span>
 	      		<span class="f14" style="color: #9B9B9B;">{{choiceItem.members.length}}人</span>
 	      		<i :class="['iconfont', 'right', 'f20', index === showingIndex ? 'icon-fold' : 'icon-unfold']" v-if="problemResultDetailData.problem_type !== 8"></i>
