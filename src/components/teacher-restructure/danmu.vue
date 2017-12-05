f4ad74bcab0dc6e8c9658f0728f4df33e74a1a98
liujunyang <liujunyangscript@gmail.com>
2017-12-05 17:14:32 +0800

    fix 当弹幕投稿列表超过一屏时，点击进入列表时有些抖动

--- src/components/teacher-restructure/danmu.vue
+++ src/components/teacher-restructure/danmu.vue
@@ -175,7 +175,8 @@
 
         if (!self.dataList[0]) {
           setTimeout(() => {
-            this.$refs.Loadmore.onBottomLoaded()
+            // this.$refs.Loadmore.onBottomLoaded()
+            self.onBottomLoaded()
           }, 100)
           return;
         }
@@ -191,9 +192,28 @@
           }
           self.dataList = self.dataList.concat(jsonData.data.danmu_list)
 
-          this.$refs.Loadmore.onBottomLoaded()
+          // this.$refs.Loadmore.onBottomLoaded()
+          self.onBottomLoaded()
         })
       },
+      /**
+       * 复写 mint-ui loadmore 组件的 onBottomLoaded 方法
+       * 处理首次加载容器会往上偏移50px 的问题
+       *
+       */
+      onBottomLoaded () {
+        let self = this
+        var $loadmore = this.$refs.Loadmore
+        $loadmore.bottomStatus = 'pull'
+        $loadmore.bottomDropped = false
+        this.$nextTick(() => {
+          $loadmore.translate = 0
+        })
+        if (!$loadmore.bottomAllLoaded && !$loadmore.containerFilled) {
+          $loadmore.fillContainer()
+        }
+      },
+      
       /**
        * 获取弹幕数据
        *
