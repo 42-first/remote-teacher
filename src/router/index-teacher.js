ee2c8f9c5298dec0fa96cd388d69df22971850c6
liujunyang <liujunyangscript@gmail.com>
2017-12-11 19:06:44 +0800

    fix H5 遥控器支付合法url ios Android不一致的问题

--- src/router/index-teacher.js
+++ src/router/index-teacher.js
@@ -45,7 +45,10 @@ const router = new Router({
       component: CollumresultDetail
     },
     {
-      path: '/redpacket/:problemid',
+      // 解决微信确认支付路径的时候，ios 取 Landing Page， Android 取 Current Page 导致微信支付合法url认定不一致的问题
+      // 微信会把 ? 后面的全给忽略掉，所以多长都不会占用路径层级了
+      // http://get.ftqq.com/8572.get
+      path: '/redpacket?/:problemid',
       name: 'redpacket',
       component: Redpacket
     },
