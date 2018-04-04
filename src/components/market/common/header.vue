b9d424e1979bab7a527bf18857150b8503aee3a7
chouzhenyi <zzyatbeijing@163.com>
2018-04-04 18:54:15 +0800

    导航

--- src/components/market/common/header.vue
+++ src/components/market/common/header.vue
@@ -2,19 +2,15 @@
   <nav id="nav" class="nav">
     <div id="navbg" class="bg"></div>
     <div class="box clearfix">
-      <a href="/">
-        <img class="logo" src="~images/market/icon/logo.png" alt="logo">
+      <a class="a_logo" href="/">
+        <img class="logo" src="http://sfe.ykt.io/o_1bpfrptlcaflochlas11h1s1c9.png" alt="logo">
       </a>
-      <!--<a class="right header" v-if="name">
-        <img :src="avatar"/>
-        <span>{{name}}</span>
-      </a>-->
-      <!--<a href="/join_us" class="right">加入我们</a>-->
-      <a href="/help" class="right right0">帮助中心</a>
+      <a href="/web" target="_blank" class="right loginweb">登录网页版</a>
+      <a href="/help" class="right">帮助中心</a>
+      <a href="/lesson/market/courseware" id="rainMarket" class="right">雨课件</a>
+      <a href="/presenter" class="right">翻页笔</a>
       <a href="/download" class="right">下载</a>
-      <a href="/lesson/market/courseware" class="right color_curr">雨课件</a>
-      <a href="/flippen" class="right">翻页笔</a>
-      <a href="/web" class="right" target="_blank">雨课堂网页版</a>
+      <a href="/" class="right active">首页</a>
     </div>
   </nav>
 </template>
@@ -53,74 +49,33 @@
 <style lang="scss" scoped>
   @import "~@/style/market/insert.css";
   /*导航条*/
-  .nav {
-    position: fixed;
-    z-index: 50;
-    width: 100%;
-    left: 0;
-    top: 0;
-    color: #9b9b9b;
-    background-color: #282C2F;
-    font: 12px/1.5 "PingFangSC-Regular",arial,Helvetica Neue,tahoma,"Hiragino Sans GB",Microsoft YaHei,\5b8b\4f53;
-    -webkit-font-smoothing: subpixel-antialiased;
+  /*导航条background-color: #282C2F; */
+  .nav {position: fixed; z-index: 50; width: 100%; left: 0; top: 0; color: #fff;background-color:rgba(0,0,0,0.8);}
+  .nav .bg {position: absolute; left: 0; top: 0; width: 100%; height: 100%; display: none;}
+  .nav .box {position: relative; width: 1170px; margin: 0 auto; height: 74px;}
+  .nav .box .logo {float: left; width: 94px; margin-top: 22px;}
+
+  .nav .box .right {float: right; margin-left: 50px;margin-top:20px;   font-size: 13px; padding-top: 1px; box-sizing: border-box; height: 34px;line-height: 34px;}
+  .nav .box .beta{width: 29px; vertical-align: super;}
+  .nav .box a {color: #9B9B9B;display: inline-block;text-align: center;border-radius: 5px;}
+  .nav .box a.active{color: #ffffff;}
+  .nav .box a:hover{color: #ffffff;}
+  .nav .box a.loginweb{
+    background-color: #5096F5;color: #fff;
+    width: 100px;
   }
 
-  .nav .bg {
-    position: absolute;
-    left: 0;
-    top: 0;
-    width: 100%;
-    height: 100%;
-    background-color: #282C2F;
-    display: none;
-  }
 
-  .nav .box {
-    position: relative;
-    width: 1080px;
-    margin: 0 auto;
-    height: 60px;
-  }
+  .contentbox{ width: 1170px;margin: 0 auto; }
+  .flex{display: flex;}
+  .flexcol{flex: 1;}
+  .tl{text-align: left;}
+  .tr{text-align: right;}
 
-  .nav .box .logo {
-    float: left;
-    width: 94px;
-    margin-top: 15px;
-  }
 
-  .nav .box .right {
-    float: right;
-    margin-right: 20px;
-    line-height: 60px;
-    font-size: 15px;
-    padding-top: 1px;
-    box-sizing: border-box;
-  }
-  .nav .box .right0{
-    margin-right: 0;
-  }
-  .nav .box .beta {
-    width: 29px;
-    vertical-align: super;
-  }
-
-  .nav .box a {
-    color: #9b9b9b;
-  }
-  .nav .box a.color_curr{
-    color: #fff;
-  }
-  .nav .box .header {
-    display: inline-block;
-    height: 100%;
-    line-height: 60px;
-    margin-left: 40px;
-    img {
-      width: 42px;
-      height: 42px;
-      border-radius: 50%;
-      vertical-align: middle;
-    }
+  .section2{
+    background-color: #151527;
+    color: #ffffff;
   }
 
 </style>
