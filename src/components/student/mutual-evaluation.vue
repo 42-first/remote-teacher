c9f89d2cd0fd9eeab41c401f9b5e95b621afdb27
陈舟 <chenzhou@xuetangx.com>
2018-09-10 16:47:40 +0800

    互评问题处理

--- src/components/student/mutual-evaluation.vue
+++ src/components/student/mutual-evaluation.vue
@@ -431,8 +431,8 @@
 
   .score__wrap:after {
     content: '';
-    // position: fixed;
-    position: absolute;
+    position: fixed;
+    // position: absolute;
     top: 0;
     bottom: 0;
     left: 0;
@@ -443,8 +443,8 @@
 
   .score__modal {
     z-index: 1;
-    // position: fixed;
-    position: absolute;
+    position: fixed;
+    // position: absolute;
     bottom: 0;
     left: 0;
     right: 0;
