51b2109b6674fb6b7a04550e52cb6aaf95bd1ec6
tuqiushuang <tuqiushuang@xuetangx.com>
2018-11-02 18:42:26 +0800

    填空题选择题result页合并bug修改

--- src/components/teacher-restructure/objectiveresult.vue
+++ src/components/teacher-restructure/objectiveresult.vue
@@ -18,6 +18,7 @@
         :members="members" 
 				:problemid="problemid" 
 				:isTouping="isTouping"
+				:problemtype="problemType"
 				@showresult = "showresult" 
         @yanshi="yanshi"
         @shouti="shouti"
@@ -66,7 +67,7 @@
 	        <div class="btn-desc f14">{{ $t('viewdetails') }}</div>
 	      </router-link>
 
-	      <router-link tag="div" :to="{name: 'redpacket', query: { problemid: problemid }}" v-show="!~problemType.indexOf('Polling') && !~RedEnvelopeID" class="btn-item">
+	      <router-link tag="div" :to="{name: 'redpacket', query: { problemid: problemid }}" v-show="!~problemType.indexOf('Polling') && !~RedEnvelopeID && !~problemType.indexOf('FillBlank')" class="btn-item">
 	        <div class="iconbox" style="background: #E64340;">
 	      	  <i class="iconfont icon-shiti_hongbao f28" style="color: #DCBC83;"></i>
 	      	</div>
