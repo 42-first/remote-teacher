d2efa9d8cdf6cadbbae897dc47bc9acc2dd69f5d
陈舟 <chenzhou@xuetangx.com>
2020-03-31 15:41:14 +0800

    音频直播增加loading

--- src/components/student/live-mixin.js
+++ src/components/student/live-mixin.js
@@ -144,7 +144,7 @@ let liveMixin = {
         this.handleerror(hls);
         this.hls = hls;
 
-        this.setLiveTip();
+        // this.setLiveTip();
       }
       // hls.js is not supported on platforms that do not have Media Source Extensions (MSE) enabled.
       // When the browser has built-in HLS support (check using `canPlayType`), we can provide an HLS manifest (i.e. .m3u8 URL) directly to the video element throught the `src` property.
