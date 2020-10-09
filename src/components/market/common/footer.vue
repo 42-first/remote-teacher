bb6b1a9d53bd1840cb70c733dc1b529289fce02a
周正义 <zhouzhengyi@xuetangx.com>
2020-10-09 11:45:06 +0800

    备案号

--- src/components/market/common/footer.vue
+++ src/components/market/common/footer.vue
@@ -10,7 +10,10 @@
           <div class="footer-cr">
             <p>©2019 清华大学&学堂在线</p>
             <p>
-            <a target="_blank" href="http://www.beian.gov.cn/portal/registerSystemInfo?recordcode=11010802033054" style="display:inline-block;vertical-align: middle;color: #666;text-decoration: none;vertical-align: middle;"><img src="https://qn-sfe.yuketang.cn/o_1ek0bqrhrrj69vi1q681h091k069.png" style="vertical-align: middle;"/><span>京公网安备 11010802033054号</span></a>
+            <a target="_blank" href="http://www.beian.gov.cn/portal/registerSystemInfo?recordcode=11010802033054" style="display:inline-block;vertical-align: middle;color: #666;text-decoration: none;vertical-align: middle;">
+            京ICP证140571号
+            <img src="https://qn-sfe.yuketang.cn/o_1ek0bqrhrrj69vi1q681h091k069.png" style="vertical-align: middle;"/><span style="vertical-align: middle;">京公网安备 11010802033054号</span>
+            </a>
             <span>| 北京慕华信息科技有限公司</span>
             </p>
           </div>
