eaa28c203b87300d9d375dc6c1af3144754230e9
陈舟 <chenzhou@xuetangx.com>
2020-12-31 18:35:30 +0800

    web开课会议

--- src/components/meeting/top-bar.vue
+++ src/components/meeting/top-bar.vue
@@ -9,8 +9,8 @@
 <template>
   <!-- header -->
   <section class="top-bar__container box-between">
-    <div class="box-center" @click="setMeetingLayout(MeetingMode.DEFAULT)">
-      <div class="action__back box-center pointer">
+    <div class="box-center pointer" @click="setMeetingLayout(MeetingMode.DEFAULT)">
+      <div class="action__back box-center">
         <svg class="icon f28 c666" aria-hidden="true">
           <use xlink:href="#icon16-zuo"></use>
         </svg>
