8e0158f7b9404b4ea978f51593309b04b6efba21
陈舟 <chenzhou@xuetangx.com>
2021-12-01 11:15:50 +0800

    风测试报错

--- build/webpack.base.conf.js
+++ build/webpack.base.conf.js
@@ -58,7 +58,10 @@ module.exports = {
         test: /\.js$/,
         loader: 'babel-loader',
         include: [resolve('src'), resolve('test')],
-        exclude: [resolve('src/util/kwai-player-v1.0.0')]
+        exclude: [resolve('src/util/kwai-player-v1.0.0')],
+        query: {
+          presets: ['es2015']
+        }
       },
       {
         test: /\.(png|jpe?g|gif|svg)(\?.*)?$/,
