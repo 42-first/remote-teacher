85dddee6027daff22879b39609e579fb15db057e
tuqiushuang <tuqiushuang@xuetangx.com>
2022-04-22 14:20:48 +0800

    --bug=1051608 --user=涂秋爽 【雨课堂】【遥控器】插件4.4开新课，插件上打开随机点名（h5遥控器实时跳转到随机点名页面），插件端关闭随机随机点名，结果：遥控器没有实时同步 https://www.tapd.cn/50384083/s/2586047

--- src/lesson/teacher/util/socket-process-message.js
+++ src/lesson/teacher/util/socket-process-message.js
@@ -451,7 +451,7 @@ function socketProcessMessage(msg){
     // 点击随机点名继续上课的回执
     if (msg.type == 'call') {
       // 随机点名页面关闭时触发的，不需要响应
-      // T_PUBSUB.publish('call-msg.callpaused', msg)
+      T_PUBSUB.publish('call-msg.closedmask', msg)
       // 随机点名的时候，继续上课没有搞懂为啥回来就存在面板遮盖，这里强制刷新一次解决
       // location.reload()
       return
