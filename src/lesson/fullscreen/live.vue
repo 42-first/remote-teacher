fa61ae0ba858cd772e4cec1eedb69e0c2221fb91
tuqiushuang <tuqiushuang@xuetangx.com>
2022-06-22 14:34:41 +0800

    --task=1064061 --user=涂秋爽 【雨课堂】直播媒体场景支持 - h5 直播页面 https://www.tapd.cn/50384083/s/2741697

--- src/lesson/fullscreen/live.vue
+++ src/lesson/fullscreen/live.vue
@@ -9,9 +9,10 @@
 
 <template>
   <section class="live__page" >
+    <div class="toLog f14" v-if="!type" >关闭直播</div>
     <video id="player" class="live__container video__container" webkit-playsinline playsinline autobuffer controls autoplay></video>
     <div class="play-btn" v-if="showBtn" @click="handlePlay">
-      <i class="iconfont icon-bofang1 cfff"></i>
+      <i class="iconfont icon-bofang1 cfff" :class="!type ? 'f40' : 'f80'"></i>
     </div>
   </section>
 </template>
@@ -25,7 +26,8 @@
     data() {
       return {
         cid: 0,
-        showBtn: true
+        showBtn: true,
+        type: 0
       }
     },
     mixins: [liveMixin],
@@ -53,7 +55,7 @@
       fetchLiveData(){
         var jsonSrc = `https://ykt-fe.yuketang.cn/liveData.js?_t=${new Date().getTime()}`
         loadScript(jsonSrc).then(res => {
-          let data = jsonData[this.cid]
+          let data = window.jsonData[this.cid]
 
           this.liveType = data.type || 1;
           this.liveurl = data;
@@ -86,6 +88,21 @@
     },
     created() {
       this.cid = this.$route.params.cid
+      this.type = this.$route.query.type || 0
+
+      if(!this.type) {
+        const addMeta = (name, content) => {
+          const meta = document.createElement('meta');
+          meta.content = content;
+          meta.name = name;
+          document.getElementsByTagName('head')[0].appendChild(meta);
+        };
+ 
+        addMeta(
+          'viewport',
+          'width=device-width,initial-scale=1,minimum-scale=1,maximum-scale=1,user-scalable=no,viewport-fit=cover',
+        );
+      }
 
       this.init()
     },
@@ -113,13 +130,26 @@
     max-height: 100%;
   }
 
+  .toLog {
+    position: absolute;
+    top: 12px;
+    left: 12px;
+    padding: 4px 13px;
+    border: 1px solid #639EF4;
+    color: #639EF4;
+    border-radius: 100px;
+  }
+
   .play-btn {
     position: absolute;
     top: 50%;
     left: 50%;
     transform: translate(-50%, -50%);
-    .iconfont {
+    .f80 {
       font-size: 80px;
+    }
+    .f40 {
+      font-size: 40px;
     }
   }
 }  
