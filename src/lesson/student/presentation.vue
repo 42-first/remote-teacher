124150b3eac6ba17fe29ff6238d5410e2f3001b4
dangkexi <dangkexi@xuetangx.com>
2022-11-28 12:32:54 +0800

    版本号

--- src/lesson/student/presentation.vue
+++ src/lesson/student/presentation.vue
@@ -1195,7 +1195,7 @@
           width: 100%;
           height: 100%;
           pointer-events: none;
-          z-index: 9;
+          z-index: 1;
         }
         .live__status {
           position: absolute;
