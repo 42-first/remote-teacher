d3175d7302a65048ef42c1c84846c1fd720aa14b
tuqiushuang <tuqiushuang@xuetangx.com>
2023-12-21 11:13:08 +0800

    --bug=1074954 --user=涂秋爽 【雨课堂】克隆班2.0，  主班未结课时， 尝试结束子班， H5没有报错提示，见下图 https://www.tapd.cn/50384083/s/3551145

--- src/lesson/teacher/common/errormsg.vue
+++ src/lesson/teacher/common/errormsg.vue
@@ -47,6 +47,7 @@
         'classroomid',
         'lessonid',
         'errType',
+        'isCloneClass',
       ])
     },
 	  methods: {
@@ -56,6 +57,14 @@
        * @event bindtap
        */
       openModal () {
+        // 克隆班不能执行当前操作
+        if (!!this.isCloneClass) {
+          this.$toast({
+            message: this.$t('cloneTips'),
+            duration: 3e3
+          });
+          return
+        }
         let self = this
 
         self.isModalHidden = false
