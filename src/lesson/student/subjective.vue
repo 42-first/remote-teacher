74fa4555a0d1aeb112b8eef8697397298c3f6228
tuqiushuang <tuqiushuang@xuetangx.com>
2024-04-17 16:32:30 +0800

    主观题作答详情兼容

--- src/lesson/student/subjective.vue
+++ src/lesson/student/subjective.vue
@@ -73,7 +73,7 @@
       <div class="subjective__answer" v-if="ispreview && result">
         <div class="answer__inner">
           <p class="answer--text f17">{{ result.content }}</p>
-          <div class="answer--image" v-if="result.pics.length && result.pics[0].pic">
+          <div class="answer--image" v-if="result.pics && result.pics.length && result.pics[0].pic">
             <img class="J_preview_img" :src="result.pics[0].pic" alt="主观题作答图片" @load="handleLoadImg(3, $event)" @click="handleScaleImage(3, $event)" />
           </div>
         </div>
